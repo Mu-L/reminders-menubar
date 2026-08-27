75d6d0cf591816336b1a788c35406e00f408b346
Rafael Damasceno <damscn@gmail.com>
2026-08-27 09:57:22 -0300

    Fix rmbLocalized call for App Store build

--- reminders-menubar/Services/UpdateService/AppStoreUpdateController.swift
+++ reminders-menubar/Services/UpdateService/AppStoreUpdateController.swift
@@ -84,9 +84,9 @@ final class AppStoreUpdateController: ObservableObject {
             if updateAvailable {
                 let alert = NSAlert()
                 alert.messageText = rmbLocalized(.updateAvailableAlertTitle)
-                alert.informativeText = rmbLocalized(.updateAvailableAlertMessage, "App Store")
+                alert.informativeText = rmbLocalized(.updateAvailableAlertMessage, arguments: "App Store")
                 alert.alertStyle = .informational
-                alert.addButton(withTitle: rmbLocalized(.openAppStoreButton, "App Store"))
+                alert.addButton(withTitle: rmbLocalized(.openAppStoreButton, arguments: "App Store"))
                 alert.addButton(withTitle: rmbLocalized(.updateLaterButton))
 
                 let response = alert.runModal()
